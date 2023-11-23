9f77bd5d17546519ba7e8788a9725554e3143646
ghifarithufail <ghifarithufail07@gmail.com>
2023-11-23 21:23:48 +0700

    scheduler status update

---
+++ app/Console/Commands/updateStatus.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Reservation;
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+
+class updateStatus extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'reservations:updateStatus';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Customer status update';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $currentDate = Carbon::now()->format('Y-m-d');
+        $allResev = Reservation::where('status', 'unpaid')->get();
+
+        foreach ($allResev as $data){
+            if($currentDate > $data->created_at){
+                $data->update(['cancel' => '0']);
+            }
+        }
+    }
+}
