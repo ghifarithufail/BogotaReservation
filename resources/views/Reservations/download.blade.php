e6ab5e292014839c0994dc9ef7ba15badf3343a9
unknown <ghifari@lakuemas.com>
2023-12-05 16:48:17 +0700

    fixed

--- resources/views/Reservations/download.blade.php
+++ resources/views/Reservations/download.blade.php
@@ -10,9 +10,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"
         integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
 
-    <title>Hello, world!</title>
 </head>
-
 <body>
     <div class="container">
         <div class="card">
@@ -20,7 +18,6 @@
                 Invoice
                 <strong>{{$reservasi->created_at->format('D d-M-Y')}}</strong>
                 <span class="float-right "> <strong>Status :</strong> {{strtoupper($reservasi->status)}}</span>
-
             </div>
             <div class="card-body">
                 <div class="row mb-4">
