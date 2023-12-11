cdc8578de5c960b7ff2273bbfb7b9e1fd76df979
ghifarithufail <ghifarithufail07@gmail.com>
2023-12-11 20:16:54 +0700

    update

--- app/Http/Controllers/fe/ContusController.php
+++ app/Http/Controllers/fe/ContusController.php
@@ -3,11 +3,17 @@
 namespace App\Http\Controllers\fe;
 
 use App\Http\Controllers\Controller;
+use App\Models\Critic;
 use Illuminate\Http\Request;
 
 class ContusController extends Controller
 {
     public function index(){
+
+        return view("frontEnd.contus");
+    }
+
+    public function data(){
 
         return view("frontEnd.contus");
     }
